b8b65b9b4cb0fede989da7faaedf2ea7202c6085
taekkim <taekkim@student.42seoul.kr>
2022-02-01 00:37:07 +0900

    feat(app): input, predict to output

--- app/app.py
+++ app/app.py
@@ -1,4 +1,12 @@
-import streamlit
+import streamlit as st
 import requests
 
-response = requests()
+sepal_l = st.number_input('꽃받침의 길이는?')
+sepal_w = st.number_input('꽃받침의 너비는?')
+petal_l = st.number_input('꽃잎의 길이는?')
+petal_w = st.number_input('꽃잎의 너비는?')
+
+if st.sidebar.button('predict'):
+    response = requests.post('http://127.0.0.1:5000/predict',
+                            json=[sepal_l, sepal_w, petal_l, petal_w])
+    st.write(response.text)
