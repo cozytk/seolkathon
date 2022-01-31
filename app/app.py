8f89e57dc3494c29036f4ed64f86f7eaa58814d1
taekkim <taekkim@student.42seoul.kr>
2022-02-01 00:59:50 +0900

    feat(app): spinner and success

--- app/app.py
+++ app/app.py
@@ -5,8 +5,12 @@ sepal_l = st.number_input('꽃받침의 길이는?')
 sepal_w = st.number_input('꽃받침의 너비는?')
 petal_l = st.number_input('꽃잎의 길이는?')
 petal_w = st.number_input('꽃잎의 너비는?')
+species = ['setosa', 'versicolor', 'virginica']
 
 if st.sidebar.button('predict'):
-    response = requests.post('http://127.0.0.1:5000/predict',
-                            json=[sepal_l, sepal_w, petal_l, petal_w])
-    st.write(response.text)
+    with st.spinner('Wait for it...'):
+        response = requests.post('http://127.0.0.1:5000/predict',
+                                json=[sepal_l, sepal_w, petal_l, petal_w])
+        label = int(response.text)
+        st.write(species[label])
+    st.success('Done!')
