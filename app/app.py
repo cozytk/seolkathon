932c68ea9138919d6bd4c11d40c96e4f6b998b23
taekkim <taekkim@student.42seoul.kr>
2022-02-01 22:14:39 +0900

    chore: gitignore

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+from os import environ
 
 sepal_l = st.number_input('꽃받침의 길이는?')
 sepal_w = st.number_input('꽃받침의 너비는?')
@@ -9,7 +10,10 @@ species = ['setosa', 'versicolor', 'virginica']
 
 if st.sidebar.button('predict'):
     with st.spinner('Wait for it...'):
-        response = requests.post('http://127.0.0.1:5000/predict',
+        # response = requests.post(f'http://127.0.0.1:5000/predict',
+        response = requests.post(f'http://model:5000/predict',
+        # response = requests.post(f'http://192.168.0.11:5000/predict',
+        # response = requests.post(f'http://{environ["MY_PUBLIC_IP"]}:5000/predict',
                                 json=[sepal_l, sepal_w, petal_l, petal_w])
         label = int(response.text)
         st.write(species[label])
